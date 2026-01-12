7622b8e400d3ebaf9c5932a146adafa1062be745
Kato <143938160+JTG-RX@users.noreply.github.com>
2026-01-11 20:07:41 -0500

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,46 +1,110 @@
+import json
+DATA_FILE = "users.json"
 
 def encoder (Password):
         encoded = []
-        ABCs = """ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 """
-        for n in Password: # this make every thing lower case to mach the ABCs
-            for i in range(len(ABCs)): # make the AbBCs in to number for 0,26
-                if n == ABCs[i]:#  if a letter in my name is the same as the letter in the ABCs then to multpy it by 3
+        ABCs = """ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890<>/?{ }[]()_-!@#$%^&*~`,.:;'|"""
+        for n in Password: 
+            for i in range(len(ABCs)): 
+                if n == ABCs[i]:
                     z = i + 4
                     encoded.append(z*3)
         Pass = sum(encoded)
         return Pass
 
-class Athentication:
+class Authentication:
     def __init__(self):
-        self.users = {}
         self.current_user = None
         self.loaded = False
-        self.encoder = encoder 
+        self.encoder = encoder
+        self.load_users()
 
     def Register(self,):
         register_User = input("-Enter Username-")
         register_Pass = input("-Enter Password-")
         encodedPass = self.encoder(register_Pass)
-        self.users[register_User] = encodedPass
-        print(self.users)
+        self.users[register_User] = {"password":encodedPass,"balance":454,"records":[]}
+        self.save_users()
+        return register_User, register_Pass
 
     def LoadCheck (self,User,Pass,):
-        if self.loaded is False :
             if User not in self.users:
                 print("you do not have an account")
                 self.loaded = False
                 return self.loaded
             else:
-                if self.encoder(Pass) == self.users[User]:
+                if self.encoder(Pass) == self.users[User]["password"]:
                     self.loaded = True
-                    print(self.users)
+                    self.current_user = User
                     return self.loaded
                 else:
                      print("""wrong password""")
                      self.loaded = False
                      return self.loaded
-        
-Bank = Athentication()
+                
+    def deposit(self,):
+        depositCheck = False
+        while depositCheck == False:
+            Deposits = int(input("How much do you want to deposit: numbers only:>"))
+            if Deposits <= 0:
+                print("youcan not deposit 0 or a negative Balance")
+            else:
+                depositCheck = True
+        self.users[self.current_user]["records"].append(f"{self.current_user} deposited {Deposits}")
+        self.users[self.current_user]['balance'] += Deposits
+        self.save_users()
+        print(f"you now have{self.users[self.current_user]['balance']}")
+
+    def Withdraw(self,):
+        WithdrawCheck = False
+        while WithdrawCheck == False:
+            Withdraw = int(input("How much do you want to Withdraw: numbers only:>"))
+            self.users[self.current_user]["records"].append(f"{self.current_user} Withdraw {Withdraw}")
+            if Withdraw >= self.users[self.current_user]["balance"]:
+                print("you can not withdraw mover than your balance")
+            elif Withdraw <= 0:
+                print("you can not withdraw a negative amout")
+            else:
+                WithdrawCheck = True   
+        self.users[self.current_user]['balance'] -= Withdraw
+        self.save_users()
+        print(f"you now have{self.users[self.current_user]['balance']}")
+    
+    def mainMenu(self,):
+        Quit = False
+        while Quit == False:
+            DW = input("""You can do the following \n -if you want to Deposit enter <Deposit> \n -if you want to Withdraw enter <Withdraw> \n -if you want to see your Record enter <Record>
+                        \n- if you want to see your balance <Balance>\n -or if you want to quit enter <Q>""")
+            self.users[self.current_user]["records"].append(f"main Menu actions {DW}")
+            if DW == "Deposit":
+                self.deposit()
+            elif DW == "Withdraw":
+                self.Withdraw()
+            elif DW == "Balance":
+                print(f"you balance is {self.users[self.current_user]['balance']}")
+            elif DW == "Q": 
+                self.save_users()   
+                Quit = True
+                self.loaded = False
+            elif DW == "Record":
+                for r in self.users[self.current_user]["records"]:
+                    print(r)
+            else:
+                print("please enter a valid option")
+    def load_users(self,):
+        try:
+            with open(DATA_FILE,'r') as file:
+                self.users = json.load(file)
+            for user_data in self.users.values():
+                if "records" not in user_data or not isinstance(user_data["records"], list):
+                    user_data["records"] = []
+        except FileNotFoundError:
+            self.users = {}
+    def save_users(self,):
+        with open(DATA_FILE,"w") as file:
+            json.dump(self.users, file, indent=4)
+   
+Bank = Authentication()
 while Bank.loaded == False:        
     print("JYK BANK")
 
@@ -50,8 +114,14 @@ while Bank.loaded == False:
         User = input("Enter Username")
         Pass = input("Enter Password")
         Bank.LoadCheck(User,Pass)
+        if Bank.loaded == True:
+            print(f"""welcome{User} \n 
+                  your balance is {Bank.users[User]['balance']} """)
+            Bank.mainMenu()
     if LogOrSign == 1:
-        Bank.Register()
-        Money = int(input("how much money do you want deposit"))
-
-#add away to add money/ also use the corint user
+        username,password = Bank.Register()
+        Bank.LoadCheck(username,password)
+        if Bank.loaded == True:
+            print(f"""welcome {username} \n 
+                  your balance is {Bank.users[username]['balance']} """)
+            Bank.mainMenu() 
